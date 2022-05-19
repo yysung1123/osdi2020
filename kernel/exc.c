3e53878330e2d7e2f9f98dc7ba54160715e79534
Yuan-Yao Sung <sungyuanyao@gmail.com>
2022-05-19 16:55:11 +0800

    lab3: add exception_table and exception_handler

---
+++ kernel/exc.c
@@ -0,0 +1,19 @@
+#include <include/printk.h>
+#include <include/types.h>
+
+void exception_handler() {
+    uint64_t ELR_EL2;
+    uint32_t ESR_EL2;
+    __asm__ volatile("mrs %0, ELR_EL2"
+                     : "=r"(ELR_EL2));
+    __asm__ volatile("mrs %0, ESR_EL2"
+                     : "=r"(ESR_EL2));
+    uint32_t EC = ESR_EL2 >> 26;
+    uint32_t ISS = ESR_EL2 & 0x01ffffff;
+
+    pl011_uart_printk("Exception return address: 0x%x\n", ELR_EL2);
+    pl011_uart_printk("Exception class (EC): 0x%x\n", EC);
+    pl011_uart_printk("Instruction specific syndrome (ISS): 0x%x\n", ISS);
+
+    for (;;) {}
+}
