2bc2d21403de4b5f8ba9495faf29a9ffd4b13a12
Yuan-Yao Sung <sungyuanyao@gmail.com>
2022-05-21 11:13:15 +0800

    lab3: switch from EL2 to EL1 on boot

--- kernel/exc.c
+++ kernel/exc.c
@@ -2,16 +2,16 @@
 #include <include/types.h>
 
 void print_exception_info() {
-    uint64_t ELR_EL2;
-    uint32_t ESR_EL2;
-    __asm__ volatile("mrs %0, ELR_EL2"
-                     : "=r"(ELR_EL2));
-    __asm__ volatile("mrs %0, ESR_EL2"
-                     : "=r"(ESR_EL2));
-    uint32_t EC = ESR_EL2 >> 26;
-    uint32_t ISS = ESR_EL2 & 0x01ffffff;
+    uint64_t ELR_EL1;
+    uint32_t ESR_EL1;
+    __asm__ volatile("mrs %0, ELR_EL1"
+                     : "=r"(ELR_EL1));
+    __asm__ volatile("mrs %0, ESR_EL1"
+                     : "=r"(ESR_EL1));
+    uint32_t EC = ESR_EL1 >> 26;
+    uint32_t ISS = ESR_EL1 & 0x01ffffff;
 
-    pl011_uart_printk("Exception return address: 0x%x\n", ELR_EL2);
+    pl011_uart_printk("Exception return address: 0x%x\n", ELR_EL1);
     pl011_uart_printk("Exception class (EC): 0x%x\n", EC);
     pl011_uart_printk("Instruction specific syndrome (ISS): 0x%x\n", ISS);
 }
