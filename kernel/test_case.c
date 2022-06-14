1985a229a00b348ce2d5c9dc654b1dfc4257e194
Yuan-Yao Sung <sungyuanyao@gmail.com>
2022-06-14 14:17:58 +0800

    lab4: required 1 and 2 test cases

---
+++ kernel/test_case.c
@@ -0,0 +1,27 @@
+#include <include/test_case.h>
+#include <include/types.h>
+#include <include/task.h>
+#include <include/printk.h>
+#include <include/sched.h>
+#include <include/irq.h>
+
+void __attribute__((optimize("O0"))) delay(uint32_t d) {
+    for (uint32_t i = 0; i < d; ++i) {}
+}
+
+void foo() {
+    while (1) {
+        task_t *cur = get_current();
+
+        pl011_uart_printk("Task id: %d\n", cur->id);
+        delay(20000000);
+        schedule();
+    }
+}
+
+void idle() {
+    while (1) {
+        schedule();
+        delay(20000000);
+    }
+}
