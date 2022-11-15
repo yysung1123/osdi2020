336277fe76e126fcdcc0dc43d6ca03e3093cbcc9
Yuan-Yao Sung <sungyuanyao@gmail.com>
2022-11-15 16:00:24 +0800

    lab5: required 1-1: set up TCR_EL1

---
+++ include/asm/pgtable.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define TCR_CONFIG_REGION_48bit (((64 - 48) << 0) | ((64 - 48) << 16))
+#define TCR_CONFIG_4KB ((0b00 << 14) |  (0b10 << 30))
+#define TCR_CONFIG_DEFAULT (TCR_CONFIG_REGION_48bit | TCR_CONFIG_4KB)
