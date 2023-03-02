1db74c1131b8464cdd7832b198e6f6dadf3e2b91
Yuan-Yao Sung <sungyuanyao@gmail.com>
2023-03-02 21:41:30 +0800

    lab5: elective 4: implement copy-on-write

--- kernel/task.c
+++ kernel/task.c
@@ -42,8 +42,6 @@ struct mango_tree mtree_init_ts(uint64_t min_val, uint64_t max_val) {
 }
 
 void copy_mm(mm_struct *dst, mm_struct *src) {
-    copy_pgd(dst, src);
-
     struct mango_node *node;
     mt_for_each(&src->mt, node) {
         struct vm_area_struct *new_vma = NULL;
@@ -58,6 +56,15 @@ void copy_mm(mm_struct *dst, mm_struct *src) {
             new_vma->vm_end = vma->vm_end;
             new_vma->vm_mm = dst;
             new_vma->vm_page_prot = vma->vm_page_prot;
+
+            pte_t *ptep;
+            for (virtaddr_t va = node->index; va <= node->last; va += PAGE_SIZE) {
+                if (follow_pte(src, va, &ptep) == 0) {
+                    *ptep = __pte(pte_val(*ptep) | PD_RO);
+                    page_t *pp = pa2page(__pte_to_phys(*ptep));
+                    insert_page(dst, pp, va, __pgprot(pgprot_val(vma->vm_page_prot) | PD_RO));
+                }
+            }
         }
 
         mtree_insert_range(&dst->mt, node->index, node->last, new_vma);
